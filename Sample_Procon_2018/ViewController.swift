13873843155001fd0cffcd9beed80b2e377ac031
sekiya kosuke <i15seiya@gmail.com>
2018-10-10 19:54:44 +0900

    Initial Commit

---
+++ Sample_Procon_2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample_Procon_2018
+//
+//  Created by 関谷恒甫 on 2018/10/10.
+//  Copyright © 2018年 関谷恒甫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
